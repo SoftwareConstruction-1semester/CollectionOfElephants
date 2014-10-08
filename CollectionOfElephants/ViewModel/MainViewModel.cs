d6fb978c78fa0db2d720bf1688d3357449b97fb3
ev <ev@evang.dk>
2014-10-08 22:22:39 +0200

    command implemented

--- CollectionOfElephants/ViewModel/MainViewModel.cs
+++ CollectionOfElephants/ViewModel/MainViewModel.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using CollectionOfElephants.Annotations;
 using CollectionOfElephants.Model;
 
@@ -16,11 +17,12 @@ namespace CollectionOfElephants.ViewModel
         private ObservableCollection<ElephantModel> _elephants;
         private ElephantModel _selectedElephantModel;
         private ElephantModel _newElephaneModel;
+        private ICommand _addNewElephant;
 
         public MainViewModel()
         {
             _elephants = new ObservableCollection<ElephantModel>();
-            
+   
             ElephantModel e1 = new ElephantModel();
             e1.EarSize = "Big";
             e1.Name = "Monty";
@@ -31,14 +33,29 @@ namespace CollectionOfElephants.ViewModel
             e2.Name = "Python";
             e2.imageURL = "/Assets/elephants-9a.jpg";
 
-            // short way of doing the sam as above
+            // short way of doing the same as above
             ElephantModel e3 = new ElephantModel(){EarSize = "small", Name = "Ebbe", NumberOfChildren = 2, Weight = 78, imageURL = ""};
 
             _elephants.Add(e1);
             _elephants.Add(e2);
             _elephants.Add(e3);
 
-           // SelectedElephantModel = _elephants[0];
+            _newElephaneModel = new ElephantModel();
+            _addNewElephant = new RelayCommand(AddElephant);
+
+
+        }
+        //executed by the a relaycommand (addNewElephant)
+        private void AddElephant()
+        {
+            _elephants.Add(_newElephaneModel);
+            OnPropertyChanged("Elephants");
+        }
+
+        public ICommand AddNewElephant
+        {
+            get { return _addNewElephant; }
+            set { _addNewElephant = value; }
         }
 
         public ElephantModel NewElephaneModel
